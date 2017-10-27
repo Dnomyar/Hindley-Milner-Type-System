156d134ab673e4a1c0771f26fe987fd787f71dca
Damien RAYMOND <dam.ray49@gmail.com>
2017-10-27 16:45:24 +0200

    Add a generalizer

---
+++ src/main/java/fr/imt/inference/Generalizer.java
@@ -0,0 +1,13 @@
+package fr.imt.inference;
+
+import fr.imt.inference.Environment;
+import fr.imt.inference.type.Type;
+
+public class Generalizer {
+
+
+    // returns Scheme ?
+    public Type generalize(Environment env, Type type) {
+        return type; //todo
+    }
+}
