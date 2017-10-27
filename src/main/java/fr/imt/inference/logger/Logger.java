584cb46cae287655479e8363aff7b7f16d6d050e
Damien RAYMOND <dam.ray49@gmail.com>
2017-10-27 18:29:03 +0200

    Make colors attributes private

--- src/main/java/fr/imt/inference/logger/Logger.java
+++ src/main/java/fr/imt/inference/logger/Logger.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 public class Logger {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
 
-    public static final String ANSI_BLACK_BOLD_BRIGHT = "\033[1;90m";
+    private static final String ANSI_BLACK_BOLD_BRIGHT = "\033[1;90m";
 
     private String className;
 
