9f361299e57ab3c70707789791208067733c1580
Damien RAYMOND <dam.ray49@gmail.com>
2017-10-31 20:54:16 +0100

    Improve UnificationFailureException error message

--- src/main/java/fr/imt/inference/errors/UnificationFailureException.java
+++ src/main/java/fr/imt/inference/errors/UnificationFailureException.java
@@ -1,10 +1,10 @@
 package fr.imt.inference.errors;
 
 import fr.imt.inference.Constraint;
-import fr.imt.inference.Tuple;
 import fr.imt.inference.type.Type;
 
 public class UnificationFailureException extends Throwable {
     public UnificationFailureException(Constraint constraint) {
+        super("UnificationFailureException : Cannot unify type `" + constraint.left + "` with type `" + constraint.right + "`");
     }
 }
