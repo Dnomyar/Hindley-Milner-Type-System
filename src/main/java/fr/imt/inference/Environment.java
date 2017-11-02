0fb89f1fe5dc1a2e13e7ea5b21841e01c6e16775
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2017-11-02 15:54:32 +0100

    Vavr for environment

--- src/main/java/fr/imt/inference/Environment.java
+++ src/main/java/fr/imt/inference/Environment.java
@@ -4,32 +4,30 @@ import fr.imt.inference.AST.Expression;
 import fr.imt.inference.AST.Variable;
 import fr.imt.inference.logger.Logger;
 import fr.imt.inference.type.Type;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class Environment {
 
     private Logger logger = new Logger();
-
-
     private Map<Expression, Type> env;
 
     public Environment() {
-        this.env = new HashMap<>();
+        this.env = HashMap.empty();
     }
 
     public void extend(Expression expression, Type type) {
         logger.trace("Extend env: " + expression + " -> " + type);
-        this.env.put(expression, type);
+        this.env = this.env.put(expression, type);
     }
 
     public void remove(Expression expression) {
-        logger.trace("Remove : " + expression + " linked to " + this.env.get(expression) + " from env.");
-        this.env.remove(expression);
+        logger.trace("Remove : " + expression + " linked to " + this.env.get(expression).get() + " from env.");
+        this.env = this.env.remove(expression);
     }
 
     public Type get(Variable variable) {
-        return this.env.get(variable);
+        return this.env.get(variable).get();
     }
 }
